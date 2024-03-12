30fd4ab5d3a1c8b7bab8e145949e14a862e943dd
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-03-12 03:21:21 +0200

    update package.json and make vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    esbuild: {
+        jsxFactory: 'h',
+        jsxFragment: 'Fragment',
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
+    preview: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
+    resolve: {
+        alias: {
+            "@": "/src/",
+            "@assets": "/src/assets",
+            "@components": "/src/components",
+        }
+    }
+})
